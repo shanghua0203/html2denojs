31f15b2203ad656bea688acb8fcab014129ce0bb
ccckmit <ccckmit@gmail.com>
2023-04-28 10:34:21 +0800

    add topbar jsaction

--- 01-前端/04-dom/03-navbar2/navbar2.js
+++ 01-前端/04-dom/03-navbar2/navbar2.js
@@ -8,11 +8,14 @@ aboutus/
 aboutus/
     `,
     '#example1':`
-example1/
-example1/
-example1/
-example1/
-example1/
+    <h1>註冊</h1>
+    <input type="text" placeholder="姓名"/><br/>
+    <input type="text" placeholder="帳號"/><br/>
+    <input type="email" placeholder="email"/><br/>
+    <input type="password" placeholder="密碼"/><br/>
+    <input type="text" placeholder="血型"/><br/>
+    <input type="date" placeholder="生日"/><br/>
+    <input type="color" placeholder="喜歡的顏色"/><br/>
     `,
     '#example2':`example2`,
     '#example3':'example3',
