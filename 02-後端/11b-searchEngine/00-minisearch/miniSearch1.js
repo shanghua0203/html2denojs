c6fe4aa2d570058d04ed087bd259a0d6d25cc12e
ccckmit <ccckmit@gmail.com>
2023-12-01 10:15:54 +0800

    add npm packages

--- 02-後端/11b-searchEngine/00-minisearch/miniSearch1.js
+++ 02-後端/11b-searchEngine/00-minisearch/miniSearch1.js
@@ -27,6 +27,18 @@ const documents = [
     category: 'non-fiction'
 },
 // ...and more
+{
+    id: 5,
+    title: '人工 智慧 概論',
+    text: '人工 智慧 概論',
+    category: '教科書'
+},
+{
+    id: 6,
+    title: '人工 智慧 實作 LLM 模型與 OpenAI API',
+    text: '人工 智慧 實作 LLM 模型與 OpenAI API',
+    category: '教科書'
+},
 ]
 
 let miniSearch = new MiniSearch({
@@ -39,8 +51,10 @@ miniSearch.addAll(documents)
 
 // Search with default options
 let results = miniSearch.search('zen art motorcycle')
+console.log(results)
 // => [
 //   { id: 2, title: 'Zen and the Art of Motorcycle Maintenance', category: 'fiction', score: 2.77258, match: { ... } },
 //   { id: 4, title: 'Zen and the Art of Archery', category: 'non-fiction', score: 1.38629, match: { ... } }
 // ]
+results = miniSearch.search('智慧')
 console.log(results)
