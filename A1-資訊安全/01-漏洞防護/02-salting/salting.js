9f9f5ad0e302b68619ce70b4ce1dbb9f730b6c2d
ccckmit <ccckmit@gmail.com>
2023-12-22 09:42:13 +0800

    modify salting.js

--- A1-資訊安全/01-漏洞防護/02-salting/salting.js
+++ A1-資訊安全/01-漏洞防護/02-salting/salting.js
@@ -1,5 +1,9 @@
-import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
 var salt = bcrypt.genSaltSync(10);
 console.log('salt=', salt)
-var hash = bcrypt.hashSync("mypassword", salt);
+var hash = bcrypt.hashSync("mypassword123", salt);
 console.log('hash=', hash)
+var result = bcrypt.compareSync("mypassword123", hash);
+console.log('result=', result)
+result = bcrypt.compareSync("abcdefg123", hash);
+console.log('result=', result)
