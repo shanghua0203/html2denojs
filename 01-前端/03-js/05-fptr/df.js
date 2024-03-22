21e9fa017d8f53e3cf532a02c948076260a80d7d
ccckmit <ccckmit@gmail.com>
2024-03-22 11:21:54 +0800

    add gd.js

--- 01-前端/03-js/05-fptr/df.js
+++ 01-前端/03-js/05-fptr/df.js
@@ -10,4 +10,5 @@ function square(x) {
 
 console.log('df(x^2,2)='+df(square, 2));
 console.log('df(x^2,2)='+df(function(x){ return x*x; }, 2));
+console.log('df(x^2,2)='+df((x)=>{ return x*x; }, 2));
 console.log('df(sin(x/4),pi/4)='+df(Math.sin, 3.14159/4));
