29d355ea4a44c20252958fe75e3386cc7695c179
shanghua <shawnshawn423423@gmail.com>
2024-12-09 00:03:59 +0800

    01oakme

---
+++ 02-後端/03-oak/HW01/oakMe.js
@@ -0,0 +1,36 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+    //ctx.response.status = 404
+    console.log('url=', ctx.request.url)
+    let pathname = ctx.request.url.pathname
+    if (pathname == '/') {
+        ctx.response.body = `<html>
+<body>
+<h1>我的自我介紹</h1>
+<ol>
+<li><a href="/name">姓名</a></li>
+<li><a href="/age">年齡</a></li>
+<li><a href="/gender">性別</a></li>
+</ol>
+</body>
+</html>
+`
+    }
+    else if (pathname == '/name') {
+        ctx.response.body = '吳尚樺'
+    }
+    else if (pathname == '/age') {
+        ctx.response.body = '19'
+    }
+    else if (pathname == '/gender') {
+        ctx.response.body = '男'
+    }else{
+        ctx.response.body = '請在網址後面輸入/naem、/age、/gender'
+    }
+});
+
+console.log('start at : http://127.0.0.1:8000/name')
+await app.listen({ port: 8000 })
